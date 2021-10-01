aba336e8c7783b33fdc9539b11108d3154d58919
shrouk-inbuco <shrouk@inbuco.me>
2021-10-01 12:04:08 +0200

    implement repositories services that get the repos data

--- src/app/modules/core/services/repositories.service.ts
+++ src/app/modules/core/services/repositories.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { format } from 'date-fns';
+import { environment } from 'src/environments/environment';
+import { Repos } from '../interfaces/repo.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoriesService {
-
-  constructor() { }
+  specificDate = format(
+    new Date(Date.now() - 30 * 24 * 60 * 60 * 1000),
+    'yyyy-MM-dd'
+  );
+  constructor(private http: HttpClient) {}
+  getRepos(page: any) {
+    return this.http.get<Repos>(
+      `${environment.baseUrl}/search/repositories?q=created:>${this.specificDate}&sort=stars&order=desc&page=${page}`
+    );
+  }
 }
