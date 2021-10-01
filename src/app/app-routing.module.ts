e73b84d5887fbee7ef1f7b54e5c3240aa317fe02
shrouk-inbuco <shrouk@inbuco.me>
2021-10-01 13:10:33 +0200

    fix the route issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LostPageComponent } from './modules/shared/components/lost-page/lost-page.component';
 
 const routes: Routes = [
   {
-    path: 'repos',
+    path: 'repos-list',
     loadChildren: () =>
       import('./modules/layouts/layouts.module').then((m) => m.LayoutsModule),
+  },
+  {
+    path: '',
+    redirectTo:'/repos-list', pathMatch: 'full' 
   },
   {
     path: '**',
